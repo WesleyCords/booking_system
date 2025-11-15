06296dfafb69f90547a6e3f77dc67c7424dc4950
Wesley Cordeiro <wesleycordeiro044@gmail.com>
2025-11-15 20:59:57 -0300

    fix: variable env

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,9 @@
+import "dotenv/config"
+
 import express from "express"
 
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 8000
 
 app.get("/wellcome", (req, res) => {
   res.send("Wellcome to the Booking System Server!")
